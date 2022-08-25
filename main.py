4d9f4a58be98be07d84991d28b3fc2cb55f899e5
Caio Silva <caio_ceac23@hotmail.com>
2022-08-25 12:25:01 -0300

    cursor

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("ola")
+import MySQLdb
+
+db = MySQLdb.connect(user="root", passwd="1234",db="treinaweb_clientes", host="localhost",port=3306)
+
+
+cursor = db.cursor()
+
+cursor.execute("SELECT * FROM cliente")
+
+print(cursor.fetchall())
+
+
+print("CONEXÃO REALIZADA COM SUCESSO")
+
+db.close()
